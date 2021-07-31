b9006279e37eca3505758048f0ba6a3107af5678
enoyat <50427196+enoyat@users.noreply.github.com>
2021-08-01 00:14:48 +0700

    update materi

--- web/workshop/routes/web.php
+++ web/workshop/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Berita;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::resource('berita', Berita::class);
